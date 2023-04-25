04c3908e31b1d7d95eba512c359d4c0880b6b2a3
Jack Humbert <jack.humb@gmail.com>
2023-04-25 13:07:07 -0400

    accept rel path for release

--- components/release.cmake
+++ components/release.cmake
@@ -3,7 +3,13 @@
 # MOD_VERSION_STR
 # MOD_GAME_DIR
 macro(configure_release ZIP_FILE)
-  set(MOD_ZIP_FILE ${ZIP_FILE})
+  set(MOD_ZIP_FILE_RAW ${ZIP_FILE})
+  cmake_path(IS_RELATIVE MOD_ZIP_FILE_RAW FILENAME_IS_RELATIVE)
+  if(${FILENAME_IS_RELATIVE})
+    set(MOD_ZIP_FILE ${MOD_SOURCE_DIR}/${MOD_ZIP_FILE_RAW})
+  else()
+    set(MOD_ZIP_FILE ${MOD_ZIP_FILE_RAW})
+  endif()
   if("${CMAKE_BUILD_TYPE}" STREQUAL "CI" AND ${PROJECT_IS_TOP_LEVEL})
     add_custom_target(ci_release
       COMMAND ${CMAKE_COMMAND} -E tar "cfv" "${MOD_ZIP_FILE}" --format=zip .
