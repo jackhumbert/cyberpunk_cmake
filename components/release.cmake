50934ebe8b79cf271f99b8e10c84c5c4219bc294
Jack Humbert <jack.humb@gmail.com>
2023-04-25 12:20:04 -0400

    CI ci_release target

--- components/release.cmake
+++ components/release.cmake
@@ -4,9 +4,17 @@
 # MOD_GAME_DIR
 macro(configure_release ZIP_FILE)
   set(MOD_ZIP_FILE ${ZIP_FILE})
-  add_custom_target(${MOD_SLUG}_release
-    COMMAND ${CMAKE_COMMAND} -E tar "cfv" "${MOD_ZIP_FILE}" --format=zip .
-    WORKING_DIRECTORY ${MOD_GAME_DIR}
-    DEPENDS ${MOD_SLUG}
-    COMMENT "Zipping game_dir for '${MOD_NAME}' ${MOD_VERSION_STR}")
+  if("${CMAKE_BUILD_TYPE}" STREQUAL "CI" AND ${PROJECT_IS_TOP_LEVEL})
+    add_custom_target(ci_release
+      COMMAND ${CMAKE_COMMAND} -E tar "cfv" "${MOD_ZIP_FILE}" --format=zip .
+      WORKING_DIRECTORY ${MOD_GAME_DIR}
+      DEPENDS ${MOD_SLUG}
+      COMMENT "Zipping game_dir for '${MOD_NAME}' ${MOD_VERSION_STR}")
+  else()
+    add_custom_target(${MOD_SLUG}_release
+      COMMAND ${CMAKE_COMMAND} -E tar "cfv" "${MOD_ZIP_FILE}" --format=zip .
+      WORKING_DIRECTORY ${MOD_GAME_DIR}
+      DEPENDS ${MOD_SLUG}
+      COMMENT "Zipping game_dir for '${MOD_NAME}' ${MOD_VERSION_STR}")
+  endif()
 endmacro()
