b284dae6a50b8586f2b23278f160f065d8074af2
Jack Humbert <jack.humb@gmail.com>
2023-04-25 13:26:02 -0400

    github_env tryout

--- components/release.cmake
+++ components/release.cmake
@@ -10,17 +10,21 @@ macro(configure_release ZIP_FILE)
   else()
     set(MOD_ZIP_FILE ${MOD_ZIP_FILE_RAW})
   endif()
-  if("${CMAKE_BUILD_TYPE}" STREQUAL "CI" AND ${PROJECT_IS_TOP_LEVEL})
-    add_custom_target(ci_release
-      COMMAND ${CMAKE_COMMAND} -E tar "cfv" "${MOD_ZIP_FILE}" --format=zip .
-      WORKING_DIRECTORY ${MOD_GAME_DIR}
-      DEPENDS ${MOD_SLUG}
-      COMMENT "Zipping game_dir for '${MOD_NAME}' ${MOD_VERSION_STR}")
-  else()
+  if(DEFINED GITHUB_ENV)
+    get_filename_component(MOD_ZIP_FILENAME ${MOD_ZIP_FILE} NAME)
+    file(APPEND "${GITHUB_ENV}" "MOD_ZIP_FILENAME=${MOD_ZIP_FILENAME}")
+  endif()
+  # if("${CMAKE_BUILD_TYPE}" STREQUAL "CI" AND ${PROJECT_IS_TOP_LEVEL})
+  #   add_custom_target(ci_release
+  #     COMMAND ${CMAKE_COMMAND} -E tar "cfv" "${MOD_ZIP_FILE}" --format=zip .
+  #     WORKING_DIRECTORY ${MOD_GAME_DIR}
+  #     DEPENDS ${MOD_SLUG}
+  #     COMMENT "Zipping game_dir for '${MOD_NAME}' ${MOD_VERSION_STR}")
+  # else()
     add_custom_target(${MOD_SLUG}_release
       COMMAND ${CMAKE_COMMAND} -E tar "cfv" "${MOD_ZIP_FILE}" --format=zip .
       WORKING_DIRECTORY ${MOD_GAME_DIR}
       DEPENDS ${MOD_SLUG}
       COMMENT "Zipping game_dir for '${MOD_NAME}' ${MOD_VERSION_STR}")
-  endif()
+  # endif()
 endmacro()
