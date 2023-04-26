d741259cd5b5dad9a137da2f0be7467e00c20279
Jack Humbert <jack.humb@gmail.com>
2023-04-26 09:54:41 -0400

    relative path for inputs

--- components/inputs.cmake
+++ components/inputs.cmake
@@ -4,7 +4,13 @@
 set(MOD_GAME_DIR_INPUT_FILE ${MOD_GAME_DIR}/r6/input/${CMAKE_PROJECT_NAME}.xml)
 
 macro(configure_inputs INPUT_FILE)
-  set(MOD_INPUTS_FILE ${INPUT_FILE})
+  set(INPUTS_FILE_RAW ${INPUT_FILE})
+  cmake_path(IS_RELATIVE INPUTS_FILE_RAW IS_INPUTS_FILE_RAW)
+  if(${IS_INPUTS_FILE_RAW})
+    set(MOD_INPUTS_FILE ${MOD_SOURCE_DIR}/${INPUTS_FILE_RAW})
+  else()
+    set(MOD_INPUTS_FILE ${INPUTS_FILE_RAW})
+  endif()
 
   message(STATUS "Configure input file: ${MOD_INPUTS_FILE}")
 
