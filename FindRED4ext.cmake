96b54afe2d5dd22311cc87f59d51fc402010b048
Jack Humbert <jack.humb@gmail.com>
2023-05-06 16:31:19 -0400

    add option for custom urls

--- FindRED4ext.cmake
+++ FindRED4ext.cmake
@@ -2,8 +2,11 @@ include(FetchContent)
 
 if(NOT DEFINED MOD_RED4EXT_DEPENDENCY_ADDED)
   if(NOT EXISTS ${MOD_BINARY_DIR}/downloads/red4ext.zip OR MOD_FORCE_UPDATE_DEPS)
+    if(NOT DEFINED MOD_RED4EXT_DOWNLOAD_URL)
+      set(MOD_RED4EXT_DOWNLOAD_URL "https://github.com/WopsS/RED4ext/releases/download/v1.12.0/red4ext_1.12.0.zip")
+    endif()
     file(DOWNLOAD
-      https://github.com/WopsS/RED4ext/releases/download/v1.12.0/red4ext_1.12.0.zip
+      ${MOD_RED4EXT_DOWNLOAD_URL}
       ${MOD_BINARY_DIR}/downloads/red4ext.zip
       EXPECTED_HASH SHA256=04CD4E22516EBBB082F2F586648E9BE97EA04FF4CCA649F28913E7B40DE087CD
       SHOW_PROGRESS
--- FindRedscript.cmake
+++ FindRedscript.cmake
@@ -2,8 +2,11 @@ include(FetchContent)
 
 if(NOT DEFINED MOD_REDSCRIPT_DEPENDENCY_ADDED)
   if(NOT EXISTS ${MOD_BINARY_DIR}/downloads/redscript.zip OR MOD_FORCE_UPDATE_DEPS)
+    if(NOT DEFINED MOD_REDSCRIPT_DOWNLOAD_URL)
+      set(MOD_REDSCRIPT_DOWNLOAD_URL "https://github.com/jac3km4/redscript/releases/download/v0.5.13/redscript-v0.5.13.zip")
+    endif()
     file(DOWNLOAD
-      https://github.com/jac3km4/redscript/releases/download/v0.5.13/redscript-v0.5.13.zip
+      ${MOD_REDSCRIPT_DOWNLOAD_URL}
       ${MOD_BINARY_DIR}/downloads/redscript.zip
       SHOW_PROGRESS
     )
