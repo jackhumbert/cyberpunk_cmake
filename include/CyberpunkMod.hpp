3017bab6398ada0b7c7e1cf90017c48473186b57
Jack Humbert <jack.humb@gmail.com>
2023-05-18 17:22:01 -0400

    better tracking

--- include/CyberpunkMod.hpp
+++ include/CyberpunkMod.hpp
@@ -1,15 +1,54 @@
 #include <stdint.h>
 #include <winnt.h>
+#include <map>
+#include <RED4ext/CName.hpp>
 
 namespace CyberpunkMod {
 
-LARGE_INTEGER recentCallTimes[0x10000] = {0};
-uint16_t recentCallTimesIndex = 0;
-// std::mutex recentCallTimesMutex;
+struct TimeTracker {
+  LONGLONG Difference() {
+    return Current.QuadPart - Last.QuadPart;
+  }
+
+  void Update(LONGLONG elapsedTicks) {
+    recentCallTimes[recentCallTimesIndex++] = elapsedTicks;
+    if (!recentCallTimesIndex) {
+      flipped = true;
+    }
+  }
+
+  LONGLONG Sum(LONGLONG frequency) {
+    LONGLONG sum = 0;
+    auto length = flipped ? 0x10000 : (recentCallTimesIndex + 1);
+    for (uint32_t i = 0; i < length; i++) {
+      sum += recentCallTimes[i];
+    } 
+    sum /= length;
+    sum *= 1000000;
+    sum /= frequency;
+    return sum;
+  }
+
+  LARGE_INTEGER Last;
+  LARGE_INTEGER Current;
+  LONGLONG recentCallTimes[0x10000] = {0};
+  uint16_t recentCallTimesIndex = 0;
+  bool flipped;
+};
+
+std::map<RED4ext::CName, TimeTracker> timeTrackers;
 
 struct Profiler {
-  inline Profiler() {
+  inline Profiler(RED4ext::CName tracker, uint32_t printInterval_s) : Tracker(tracker) {
     QueryPerformanceFrequency(&Frequency); 
+    if (timeTrackers.find(Tracker) == timeTrackers.end()) {
+      timeTrackers[Tracker] = TimeTracker();
+    }
+    timeTrackers[Tracker].Last = timeTrackers[Tracker].Current;
+    QueryPerformanceCounter(&timeTrackers[Tracker].Current);
+    if (timeTrackers[Tracker].Difference() > (Frequency.QuadPart * printInterval_s)) {
+      shouldPrint = true;
+    }
     QueryPerformanceCounter(&StartingTime);
   }
 
@@ -17,24 +56,18 @@ struct Profiler {
     QueryPerformanceCounter(&EndingTime);
     LARGE_INTEGER ElapsedMicroseconds;
     ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
-    // std::lock_guard<std::mutex> recentCallTimesLock(recentCallTimesMutex);
-    recentCallTimes[recentCallTimesIndex++] = ElapsedMicroseconds;
-    if (recentCallTimesIndex == 0) {
-      LARGE_INTEGER sum = LARGE_INTEGER();
-      for (uint32_t i = 0; i < 0x10000; i++) {
-        sum.QuadPart += recentCallTimes[i].QuadPart;
-      } 
-      sum.QuadPart /= 0x10000;
-      sum.QuadPart *= 1000000;
-      sum.QuadPart /= Frequency.QuadPart;
-      return sum.QuadPart;
+    timeTrackers[Tracker].Update(ElapsedMicroseconds.QuadPart);
+    if (shouldPrint) {
+      return timeTrackers[Tracker].Sum(Frequency.QuadPart);
     }
     return 0;
   }
 
+  RED4ext::CName Tracker;
   LARGE_INTEGER Frequency;
   LARGE_INTEGER StartingTime;
   LARGE_INTEGER EndingTime;
+  bool shouldPrint;
 };
 
 } // namespace CyberpunkMod
