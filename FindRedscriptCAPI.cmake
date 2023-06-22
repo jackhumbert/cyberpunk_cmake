56c44f04f395042a1a7dd8d2e878be711b3cc141
Jack Humbert <jack.humb@gmail.com>
2023-06-22 09:54:54 -0400

    update capi location

--- FindRedscriptCAPI.cmake
+++ FindRedscriptCAPI.cmake
@@ -5,11 +5,11 @@ set_target_properties(RedscriptCAPI PROPERTIES
   FOLDER "Dependencies" 
   LINKER_LANGUAGE C 
   # OUTPUT_NAME redscript_capi
-  IMPORTED_LOCATION "${RESCRIPT_CAPI_DIR}/target/release/redscript_capi.dll"
-  IMPORTED_IMPLIB "${RESCRIPT_CAPI_DIR}/target/release/redscript_capi.dll.lib"
+  IMPORTED_LOCATION "${RESCRIPT_CAPI_DIR}/redscript_capi.dll"
+  IMPORTED_IMPLIB "${RESCRIPT_CAPI_DIR}/redscript_capi.dll.lib"
 )
 
 
 target_include_directories(RedscriptCAPI INTERFACE "${RESCRIPT_CAPI_DIR}/src")
-configure_mod_file("${RESCRIPT_CAPI_DIR}/target/release/redscript_capi.dll" "red4ext/plugins/ctd_helper/redscript_capi.dll")
-configure_mod_file("${RESCRIPT_CAPI_DIR}/target/release/redscript_capi.pdb" "red4ext/plugins/ctd_helper/redscript_capi.pdb")
+configure_mod_file("${RESCRIPT_CAPI_DIR}/redscript_capi.dll" "red4ext/plugins/ctd_helper/redscript_capi.dll")
+configure_mod_file("${RESCRIPT_CAPI_DIR}/redscript_capi.pdb" "red4ext/plugins/ctd_helper/redscript_capi.pdb")
