698c1380921a96cb02fe9c214e02c56f5fd3aaac
Jack Humbert <jack.humb@gmail.com>
2025-07-19 11:11:50 -0400

    disable redscript dl for now

--- FindRedscript.cmake
+++ FindRedscript.cmake
@@ -1,24 +1,24 @@
 include(FetchContent)
 
 list(APPEND MOD_REQUIREMENTS "Redscript 0.5.28+")
-if(NOT DEFINED MOD_REDSCRIPT_DEPENDENCY_ADDED)
-  if(NOT EXISTS ${MOD_BINARY_DIR}/downloads/redscript.zip OR MOD_FORCE_UPDATE_DEPS)
-    if(NOT DEFINED MOD_REDSCRIPT_DOWNLOAD_URL)
-      file(DOWNLOAD
-        https://api.github.com/repos/jac3km4/redscript/releases
-        ${MOD_BINARY_DIR}/downloads/redscript_releases.json
-      )
-      file(READ ${MOD_BINARY_DIR}/downloads/redscript_releases.json REDSCRIPT_RELEASES)
-      string(JSON MOD_REDSCRIPT_DOWNLOAD_URL GET "${REDSCRIPT_RELEASES}" 0 assets 1 browser_download_url)
-    endif()
-    file(DOWNLOAD
-      ${MOD_REDSCRIPT_DOWNLOAD_URL}
-      ${MOD_BINARY_DIR}/downloads/redscript.zip
-    )
-    file(ARCHIVE_EXTRACT
-        INPUT ${MOD_BINARY_DIR}/downloads/redscript.zip
-        DESTINATION ${TOP_MOD_SOURCE_DIR}/game_dir_requirements/
-    )
-  endif()
-  set(MOD_REDSCRIPT_DEPENDENCY_ADDED ON)
-endif()
+# if(NOT DEFINED MOD_REDSCRIPT_DEPENDENCY_ADDED)
+#   if(NOT EXISTS ${MOD_BINARY_DIR}/downloads/redscript.zip OR MOD_FORCE_UPDATE_DEPS)
+#     if(NOT DEFINED MOD_REDSCRIPT_DOWNLOAD_URL)
+#       file(DOWNLOAD
+#         https://api.github.com/repos/jac3km4/redscript/releases
+#         ${MOD_BINARY_DIR}/downloads/redscript_releases.json
+#       )
+#       file(READ ${MOD_BINARY_DIR}/downloads/redscript_releases.json REDSCRIPT_RELEASES)
+#       string(JSON MOD_REDSCRIPT_DOWNLOAD_URL GET "${REDSCRIPT_RELEASES}" 0 assets 1 browser_download_url)
+#     endif()
+#     file(DOWNLOAD
+#       ${MOD_REDSCRIPT_DOWNLOAD_URL}
+#       ${MOD_BINARY_DIR}/downloads/redscript.zip
+#     )
+#     file(ARCHIVE_EXTRACT
+#         INPUT ${MOD_BINARY_DIR}/downloads/redscript.zip
+#         DESTINATION ${TOP_MOD_SOURCE_DIR}/game_dir_requirements/
+#     )
+#   endif()
+#   set(MOD_REDSCRIPT_DEPENDENCY_ADDED ON)
+# endif()
